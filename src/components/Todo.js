b8a59e41be12e3ccef056adf5675a52a8f2fb9e1
maykoco <jatuporn.smk@gmail.com>
2023-08-02 02:31:24 +0700

    first state

---
+++ src/components/Todo.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+
+function Todo() {
+
+    const [edit, setEdit] = useState({
+        id: null,
+    })
+  return (
+    <div>Todo</div>
+  )
+}
+
+export default Todo
